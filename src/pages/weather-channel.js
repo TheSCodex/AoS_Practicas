746d182c06c42271d5ae24b3644e1f69823be6b4
Alexis Dolores <alexisdanieldr@gmail.com>
2023-09-21 14:27:22 -0500

    Api agregada

--- src/pages/weather-channel.js
+++ src/pages/weather-channel.js
@@ -1,15 +1,20 @@
 import React from "react";
 import NavBar from "../components/nav-bar";
+import CondicionAtmosferica from "../components/Api";
 
 const WeatherChannel = () => {
   return (
     <>
       <div className='flex'>
-        <div className='p-[128px]'>
-          <NavBar />
-        </div>
-        <div>
-          <h1>Hola Geis!</h1>
+        <div className='flex justify-between'>
+          <div className='p-[128px]'>
+            <NavBar />
+          </div>
+          <div>
+            <div className='p-[16px]'>
+              <CondicionAtmosferica />
+            </div>
+          </div>
         </div>
       </div>
     </>
