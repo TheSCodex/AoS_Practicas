6496985037434e6dd3ec58bd92e193426340f67a
TheSCodex <eliud1102@outlook.es>
2023-09-22 13:20:19 -0500

    correcciones de estilos

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,8 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCloudRain, faSun,faCloud, faCloudSun, faCloudShowersHeavy } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCloudRain,
+  faSun,
+  faCloud,
+  faCloudSun,
+  faCloudShowersHeavy,
+} from "@fortawesome/free-solid-svg-icons";
 function CondicionAtmosferica() {
-  const url = "https://api.datos.gob.mx/v1/condiciones-atmosfericas";
+  const url =
+    "https://api.datos.gob.mx/v1/condiciones-atmosfericas?pageSize=5000";
   const estadosMx = [
     { id: 1, name: "Aguascalientes" },
     { id: 2, name: "Baja California" },
@@ -12,29 +19,29 @@ function CondicionAtmosferica() {
     { id: 6, name: "Chihuahua" },
     { id: 7, name: "Coahuila" },
     { id: 8, name: "Colima" },
-    { id: 9, name: "Ciudad de México" },
+    { id: 9, name: "Ciudad de Mexico" },
     { id: 10, name: "Durango" },
     { id: 11, name: "Guanajuato" },
     { id: 12, name: "Guerrero" },
     { id: 13, name: "Hidalgo" },
     { id: 14, name: "Jalisco" },
-    { id: 15, name: "México" },
-    { id: 16, name: "Michoacán" },
+    { id: 15, name: "Estado de Mexico" },
+    { id: 16, name: "Michoacan" },
     { id: 17, name: "Morelos" },
     { id: 18, name: "Nayarit" },
-    { id: 19, name: "Nuevo León" },
+    { id: 19, name: "Nuevo Leon" },
     { id: 20, name: "Oaxaca" },
     { id: 21, name: "Puebla" },
-    { id: 22, name: "Querétaro" },
+    { id: 22, name: "Queretaro" },
     { id: 23, name: "Quintana Roo" },
-    { id: 24, name: "San Luis Potosí" },
+    { id: 24, name: "San Luis Potosi" },
     { id: 25, name: "Sinaloa" },
     { id: 26, name: "Sonora" },
     { id: 27, name: "Tabasco" },
     { id: 28, name: "Tamaulipas" },
     { id: 29, name: "Tlaxcala" },
     { id: 30, name: "Veracruz" },
-    { id: 31, name: "Yucatán" },
+    { id: 31, name: "Yucatan" },
     { id: 32, name: "Zacatecas" },
   ];
 
@@ -59,21 +66,25 @@ function CondicionAtmosferica() {
 
   return (
     <div className="relative">
-    <div className="flex flex-col items-center">
-    <h1 className="text-xl font-bold text-white mb-5">Estado del Tiempo</h1>
-      <select
-        className="block w-full bg-white border border-gray-300 rounded-lg p-2"
-        onChange={(e) => setEstadoActual(e.target.value)}
-      >
-        <option value="">Selecciona una opción</option>
-        {estadosMx.map((opcion) => (
-          <option key={opcion.id} value={opcion.name}>
-            {opcion.name}
-          </option>
-        ))}
-      </select> 
-      </div> 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="flex flex-col items-center">
+        <h1 className="text-xl font-bold text-white mb-5">Estado del Tiempo</h1>
+        <div className="relative">
+          <div className="overflow-y-auto">
+            <select
+              className="block w-full bg-white border border-gray-300 rounded-lg mb-8 p-2"
+              onChange={(e) => setEstadoActual(e.target.value)}
+            >
+              <option value="">Selecciona una opción</option>
+              {estadosMx.map((opcion) => (
+                <option key={opcion.id} value={opcion.name}>
+                  {opcion.name}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+      </div>
+      <div className="grid border p-4 w-full overflow-y-auto max-h-[500px] rounded-md bg-white grid-cols-4 gap-4">
         {datos.map((ciudad, index) => {
           if (ciudad.state === estadoActual) {
             return (
